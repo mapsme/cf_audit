726e83eb9bbb72b9b1329aac42bb76490e468525
Ilya Zverev <zverik@textual.ru>
2017-12-28 17:34:59 +0300

    Show dataset position for created and moved points

--- www/static/audit.js
+++ www/static/audit.js
@@ -215,7 +215,7 @@ function displayPoint(data, audit) {
       rlatlon = refCoord ? L.latLng(refCoord[1], refCoord[0]) : null;
   } else if (movePos == 'dataset' && refCoord) {
     latlon = L.latLng(refCoord[1], refCoord[0]);
-    rlatlon = wereCoord ? L.latLng(wereCoord[1], wereCoord[0]) : null;
+    rlatlon = wereCoord ? L.latLng(wereCoord[1], wereCoord[0]) : coord;
     rIsOSM = true;
   } else if (movePos.length == 2) {
     latlon = L.latLng(movePos[1], movePos[0]);
@@ -259,7 +259,7 @@ function displayPoint(data, audit) {
   }
 
   $('#hint').show();
-  if (rlatlon && props['action'] != 'create') {
+  if (rlatlon && (props['action'] != 'create' || movePos)) {
     var smTitle = rIsOSM ? 'OSM location' : 'External dataset location';
     smarker1 = L.marker(rlatlon, {opacity: 0.4, title: smTitle, zIndexOffset: -100}).addTo(map1);
     smarker2 = L.marker(rlatlon, {opacity: 0.4, title: smTitle, zIndexOffset: -100}).addTo(map2);
